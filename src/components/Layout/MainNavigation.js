b35703c22b3b94fed7f771328dc0d353ec093631
dileepkumar <gurramdileepkumar96@gmail.com>
2023-03-02 21:55:46 +0530

    first commit

---
+++ src/components/Layout/MainNavigation.js
@@ -0,0 +1,46 @@
+import React,{ useContext, useState } from "react"
+import { Link } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+import AuthContext from "../../store/AuthContext";
+
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  const authCtx=useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+  const history=useHistory();
+  const logoutHandler=()=>{
+    authCtx.logout();
+    history.replace("/auth")
+  }
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+         {isLoggedIn && 
+          ( <li>
+            <Link to='/auth'>Login</Link>
+          </li>
+          )
+         }
+          {!isLoggedIn && (
+            <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          )}
+
+          {!isLoggedIn && (
+            <li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
